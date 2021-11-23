7f0cbdac4f50099eee46b20b1bea4f55eb46ace4
maxime.welcklen <max.geminiani@gmail.com>
2021-11-23 14:29:54 +0100

    add cowboy to rebar3, try very simple node definition

--- backend/src/backend_app.erl
+++ backend/src/backend_app.erl
@@ -9,8 +9,25 @@
 
 -export([start/2, stop/1]).
 
+%Register routes
 start(_StartType, _StartArgs) ->
-    backend_sup:start_link().
+    {ok, Pid} = 'backend_sup':start_link(),
+    Routes = [ {
+        '_',
+        [
+            {"/", dht_node, []}
+        ]
+    } ],
+    Dispatch = cowboy_router:compile(Routes),
+
+    NumAcceptors = 10,
+    TransOpts = [ {ip, {0,0,0,0}}, {port, 2938} ],
+    ProtoOpts = [{env, [{dispatch, Dispatch}]}],
+
+    {ok, _} = cowboy:start_http(chicken_poo_poo,
+        NumAcceptors, TransOpts, ProtoOpts),
+
+    {ok, Pid}.
 
 stop(_State) ->
     ok.
